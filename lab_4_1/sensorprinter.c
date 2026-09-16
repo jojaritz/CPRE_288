4bab94c689dc359fce8d2572b89526cfa800620a
Morales <carlos28@iastate.edu>
2026-09-15 20:22:34 -0500

    lab4 part 2 done

--- lab_4_1/sensorprinter.c
+++ lab_4_1/sensorprinter.c
@@ -40,7 +40,11 @@ int main(void) {
 //        int IR_raw_val;
 //    } cyBOT_Scan_t;
 
-    cyBOT_Scan_t *currentScan;
+    cyBOT_Scan_t currentScan;
+    currentScan.sound_dist = 0;
+    currentScan.IR_raw_val = 0;
+
+    cyBOT_Scan_t *currentScanPtr = &currentScan;
 
     while(1) {
         char got_Byte = (char)cyBot_getByte();
@@ -50,11 +54,20 @@ int main(void) {
         if(got_Byte == 'm') {
             int i = 0;
             lcd_printf("SHFVLHFSBJB");
-            for(i;i<=180;i+=2) {
-                cyBOT_Scan(i, currentScan);
-                char distance_to_char[4] = (char) currentScan->sound_dist;
+
+            char infoHeader[25] = " ";
+            strcpy(infoHeader, "Degrees   Distance (cm)\n\r");
+            int x = 0;
+            for(x; x < 25; x++) {
+                cyBot_sendByte(infoHeader[x]);
+            }
+
+            for(i; i<=180; i += 2) {
+                cyBOT_Scan(i, currentScanPtr);
+                char distance_to_char[20];
+                sprintf(distance_to_char, "%-7d   %.1f\n\r", i, currentScanPtr->sound_dist);
                 int j = 0;
-                for(j; j < 4; j++) {
+                for(j; j < 20; j++) {
                     cyBot_sendByte(distance_to_char[j]);
                 }
             }
